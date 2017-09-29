d79fb44833a87fb6dc12737be8ab20d791e311c8
Aaron Biliyok <abiliyok@gmail.com>
2017-09-29 16:14:37 +0100

    Add webpack configuration

---
+++ webpack.config.dev.js
@@ -0,0 +1,22 @@
+import path from 'path';
+
+export default {
+  devtool:'inline-source.map',
+    entry:[
+      path.resolve(__dirname,'src/index')
+    ],
+    target:'web',
+    output:{
+      path: path.resolve(__dirname,'src'),
+      publicPath:'/',
+      filename:'bundle.js'
+    },
+    plugins:[],
+    module:{
+      loaders:[
+        {test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader']},
+        {test: /\.css$/, loader: 'style-loader!css-loader'}
+      ]
+    }
+
+}
